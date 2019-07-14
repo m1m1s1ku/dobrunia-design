dcea67a77fe6c0f940482f855eae5f43e1fa19c6
Léonard <leonard@bigsool.com>
2019-07-14 18:17:50 +0200

    add missing animate on about

--- src/pages/about.ts
+++ src/pages/about.ts
@@ -4,6 +4,8 @@ import { css, property } from 'lit-element';
 
 import Page from '../core/strategies/Page';
 import Constants from '../core/constants/constants';
+import { Utils } from '../core/ui/ui';
+import { fadeWith } from '../core/animations';
 
 interface APIPage {
     id: string;
@@ -40,7 +42,8 @@ class About extends Page {
         ];
     }
 
-    public async firstUpdated(){
+    public async connectedCallback(){
+        super.connectedCallback();
         const pageR = await fetch(Constants.route('pages/about'));
         const pageData = await pageR.json();
         const response = pageData.data;
@@ -51,13 +54,20 @@ class About extends Page {
             slug: response.slug,
             content: unsafeHTML(response.content)
         };
+        this.loaded = true;
+        if(Utils.animationsReduced()){
+            return;
+        }
+        const fade = fadeWith(300, true);
+        this.page.animate(fade.effect, fade.options);
     }
 
     public render(): void | TemplateResult {
         const page = this.pageInfo as APIPage;
 
         return html`
-        <div class="about" role="main">
+        <div id="about" class="about" role="main">
+        ${!this.loaded ? html`<paper-spinner active></paper-spinner>` : html``}
         ${page ? html`
             <h1>${page.name}</h1>
             <iron-image style="width:300px; height: 300px" sizing="cover" preload src="${page.image}"></iron-image>
@@ -65,6 +75,10 @@ class About extends Page {
         ` : html``}
         </div>
         `;
+    }
+
+    public get page(){
+        return this.shadowRoot.querySelector('#about');
     }
 }
 customElements.define(About.is, About);
