49deb5b2b204b1b3d4052d29aacc498a3335fa98
Léonard <leonard@bigsool.com>
2019-10-04 21:17:28 +0200

    add missing dep

--- src/polymer.ts
+++ src/polymer.ts
@@ -1,4 +1,5 @@
 import '@polymer/iron-image/iron-image';
 import '@polymer/iron-icons/iron-icons';
+import '@polymer/paper-input/paper-input';
 import '@polymer/paper-spinner/paper-spinner';
 import '@polymer/paper-icon-button/paper-icon-button';
