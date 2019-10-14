28104f4cb8833bbe2a063201bf1fd7b2d0de73b4
Léonard <leonard@bigsool.com>
2019-10-15 00:37:50 +0200

    remove unused keys

--- src/constants.ts
+++ src/constants.ts
@@ -1,27 +1,12 @@
 import Elara from './core/elara';
-import { html } from 'lit-html';
-import { navigate } from './core/routing/routing';
 
 /**
  * App constants
  */
 const Constants = {
     domain: 'dobruniadesign.com',
-    api: 'https://base.dobruniadesign.com/wp-json/',
     graphql: 'https://base.dobruniadesign.com/graphql/',
-    proxy: location.host === 'localhost:3000' ? '' : '',
-    menu: 'dobrunia/menu',
-    login: 'jwt-auth/v1/token',
-    media: 'wp/v2/media',
-    posts: 'wp/v2/posts',
-    projects: 'wp/v2/projects',
-    tags: 'wp/v2/tags',
-    validate: 'jwt-auth/v1/token/validate',
-    categories: 'wp/v2/categories',
     title: 'Dobrunia Design',
-    logo: () => html`
-        <iron-image @click=${() => navigate('home')} style="cursor: pointer; width: 130px; height: 92px;" sizing="cover" preload src="assets/logo.png"></iron-image>
-    `,
     modes: {
         default: 'day' as Elara.Modes
     },
